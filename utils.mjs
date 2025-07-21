629e63528013b51349dcc6bde17c79d5feaab936
break <break@bincentive.com>
2025-07-21 08:22:16 +0000

    Init WASM

--- utils.mjs
+++ utils.mjs
@@ -1,4 +1,15 @@
-
 export function toAccounting(num) {
     return (parseFloat(num) || 0).toLocaleString('zh-TW');
+}
+
+export function copyText(text) {
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                console.log('Text copied to clipboard successfully');
+            })
+            .catch(err => {
+                console.error('Failed to copy text: ', err);
+            });
+    }
 }
